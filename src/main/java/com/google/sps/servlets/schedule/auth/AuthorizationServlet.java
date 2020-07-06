c1dc57e868cf22181e10750e24ee39e9354130ee
Payton Dennis <52969350+JabariD@users.noreply.github.com>
2020-07-06 17:43:50 -0500

    Remove scanner

--- src/main/java/com/google/sps/servlets/schedule/auth/AuthorizationServlet.java
+++ src/main/java/com/google/sps/servlets/schedule/auth/AuthorizationServlet.java
@@ -14,7 +14,6 @@ import javax.servlet.http.HttpServletResponse;
 
 import java.io.File;  // Import the File class
 import java.io.FileNotFoundException;  // Import this class to handle errors
-import java.util.Scanner; // Import the Scanner class to read text files
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.InputStream;
