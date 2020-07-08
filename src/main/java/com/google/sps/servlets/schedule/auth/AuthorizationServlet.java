3ce104eedde874487008475c7182a1bf8ce529c6
Payton Dennis <52969350+JabariD@users.noreply.github.com>
2020-07-08 08:52:30 -0500

    Fix Hashmap error

--- src/main/java/com/google/sps/servlets/schedule/auth/AuthorizationServlet.java
+++ src/main/java/com/google/sps/servlets/schedule/auth/AuthorizationServlet.java
@@ -3,7 +3,7 @@ package com.google.sps.servlets;
 import java.io.IOException;
 
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import javax.servlet.ServletException;
